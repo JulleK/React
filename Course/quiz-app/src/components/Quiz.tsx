d7af96e272e58fe81cc5492f3c0845658c1055c5
Julian <97186369+JulleK@users.noreply.github.com>
2025-02-12 20:42:50 +0100

    shuffling quiz answers; quiz completed screen

--- Course/quiz-app/src/components/Quiz.tsx
+++ Course/quiz-app/src/components/Quiz.tsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
-import { Answer, Question } from "../quizTypes";
+import { Answer } from "../quizTypes";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 import QUESTIONS from "../questions";
 
 const Quiz: React.FC = () => {
   const [userAnswers, setUserAnswers] = useState<Answer[]>([]);
+
   const activeQuestionIndex = userAnswers.length;
   const activeQuestion = QUESTIONS[activeQuestionIndex];
+  const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = (selectedAnswer: Answer) => {
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   };
+
+  if (quizIsOver) {
+    return (
+      <div id="summary">
+        <h2>Quiz Completed!</h2>
+        <img src={quizCompleteImg} />
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...activeQuestion.answers].sort(
+    () => Math.random() - 0.5
+  );
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{activeQuestion.text}</h2>
         <ul id="answers">
-          {activeQuestion.answers.map((answer, idx) => {
+          {shuffledAnswers.map((answer, idx) => {
             return (
               <li key={idx} className="answer">
                 <button onClick={() => handleSelectAnswer(answer)}>
